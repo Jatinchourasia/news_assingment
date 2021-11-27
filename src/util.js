57834853b84b95811f35958da56edcb819696af6
Jatinchourasia <jatinchourasia3@gmail.com>
2021-11-27 18:15:47 +0530

    page limit

--- src/util.js
+++ src/util.js
@@ -4,7 +4,7 @@ const base_url = "https://newsapi.org/v2/top-headlines";
 
 export const getLatestNews = async (country) => {
   const { data } = await axios.get(
-    `${base_url}?country=${country}&apiKey=${process.env.REACT_APP_API_KEY}`
+    `${base_url}?country=${country}&pageSize=12&apiKey=${process.env.REACT_APP_API_KEY}`
   );
   return data;
 };
